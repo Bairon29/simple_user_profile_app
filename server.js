c8c99fad5f41bde74be31f106b2e440b958352d2
Bairon29 <baironvasquez29@gmail.com>
2019-02-05 20:00:41 -0500

    Started production configuration for the server

--- server.js
+++ server.js
@@ -45,7 +45,17 @@ mongoose.connect(mongoURI, {useNewUrlParser: true})
 .catch(err => console.log('error: ' + err));
 
 app.use('/users', users);
-console.log(process.env.SECRET_KEY)
+
+//Ready for production
+// if (process.env.NODE_ENV === 'production') {
+//     // Serve any static files
+//     app.use(express.static(path.join(__dirname, 'client/build')));
+//     // Handle React routing, return all requests to React app
+//     app.get('*', function(req, res) {
+//       res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+//     });
+// }
+
 app.listen(port, () => {
     console.log('Server is running on port: ' + port);
 })
